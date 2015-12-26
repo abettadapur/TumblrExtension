8c0b445b21ce3ad612a9fcac2dd83d143b8bd509
Sara Cagle <cagle.sara@gmail.com>
2015-12-25 21:53:42 -0500

    .org pattern matching example pagemod

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ function dummy(text, callback) {
 }
 
 exports.dummy = dummy;
+
+
+// Import the page-mod API
+var pageMod = require("sdk/page-mod");
+ 
+// Create a page-mod
+// It will run a script whenever a ".org" URL is loaded
+// The script replaces the page contents with a message
+pageMod.PageMod({
+  include: "*.org",
+  contentScript: 'document.body.innerHTML = ' +
+                 ' "<h1>Page matches ruleset</h1>";'
+});
