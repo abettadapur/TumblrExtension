6dd58ce708fcfbd2b2f88d863d8ab7b81b029c55
Alex Bettadapur <alexbettadapur@gmail.com>
2015-12-26 21:55:25 -0700

    Basic script

--- data/tumblrExt.js
+++ data/tumblrExt.js
@@ -1,1 +1,14 @@
-document.body.innerHTML = "<h1>you rock motherfucker</h1>";
+var posts = $(".post_container").not(".new_post_buttons_container"); //gets all posts on the page$
+$.each(posts, function(index, post){
+	var postChild = $(".post_full", post).get(0);
+	var postJson = JSON.parse(postChild.attributes["data-json"].value);
+	var rootId = postJson['root_id'];
+	var postId = postJson['id'];
+	var reblogKey = postJson['reblog-key'];
+	var reblogLink ="/reblog/"+rootId+"/"+reblogKey;		
+
+	if(rootId != postId) { //if you are NOT the OP, create the new button
+		var buttons_div = $('.post_controls_inner', post).get(0); //grabs the buttons
+		buttons_div.innerHTML = buttons_div.innerHTML + '<a class="post_control post-control-icon reblog" target="_blank" title="Reblog from Source" href="'+reblogLink+'" data-subview="reblog"></a>'; //element text
+	}
+});
