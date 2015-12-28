3027128853051c4461235441e3068f5f73df91ff
Sara Cagle <cagle.sara@gmail.com>
2015-12-28 01:54:22 -0500

    added potential infinite scroll fixes and comments

--- data/tumblrExt.js
+++ data/tumblrExt.js
@@ -1,29 +1,67 @@
-var posts = $(".post_container").not(".new_post_buttons_container"); //gets all posts on the page$
-$.each(posts, function(index, post){
-	var postChild = $(".post_full", post).get(0);
-	if(postChild == undefined ){
-		return;
-	}
-	var postJson = JSON.parse(postChild.attributes["data-json"].value);
-	var rootId = postJson['root_id'];
-	var postId = postJson['id'];
-	var reblogKey = postJson['reblog-key'];
-	var reblogLink ="/reblog/"+rootId+"/"+reblogKey;		
+makeButtons = function(){
 
-	if(rootId != postId) { //if you are NOT the OP, create the new button
-		var buttons_div = $('.post_controls_inner', post).get(0); //grabs the button location
-		buttons_div.innerHTML = buttons_div.innerHTML + '<a class="post_control post-control-icon reblog-from-source" target="_blank" title="Reblog from Source" href="'+reblogLink+'" data-subview="reblog"></a>'; 
-	}
+	var posts = $(".post_container").not(".new_post_buttons_container"); //gets all posts on the page$
+	$.each(posts, function(index, post){
+		var postChild = $(".post_full", post).get(0);
+		if(postChild == undefined ){
+			return;
+		}
+		var postJson = JSON.parse(postChild.attributes["data-json"].value);
+		var rootId = postJson['root_id'];
+		var postId = postJson['id'];
+		var reblogKey = postJson['reblog-key'];
+		var reblogLink ="/reblog/"+rootId+"/"+reblogKey;		
+
+		if(rootId != postId) { //if you are NOT the OP, create the new button
+			var buttons_div = $('.post_controls_inner', post).get(0); //grabs the button location
+			buttons_div.innerHTML = buttons_div.innerHTML + '<a class="post_control post-control-icon reblog-from-source" target="_blank" title="Reblog from Source" href="'+reblogLink+'" data-subview="reblog"></a>'; 
+		}
+	});
+}
+
+
+
+window.onload = makeButtons();
+
+/*
+* Attempted use of ".on" function, the modern version of the ".bind" function. 
+* But I think I'm binding to the wrong event--I'm not sure what to call
+* the pagination event that we are trying to bind to.
+*/
+/*$(".posts").on("scroll", makeButtons);*/
+
+
+
+/*
+* Trying to check for infinite scroll using MutationObserver, but still working out the kinks.
+* getElementById insists that "posts" doesn't exist, when it in fact does.
+* We can't use a JQuery selector here because it doesn't create a Node object, which the target must be.
+*/
+/*MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function(mutations, observer) {
+    // fired when a mutation occurs
+    makeButtons();
+    // ...
 });
+
+var target = getElementById( "posts" );
+// define what element should be observed by the observer
+// and what types of mutations trigger the callback
+observer.observe(target, {
+	childList: true,
+	attributes: false,
+	characterData: true, //???
+	subtree: false,
+	attributeOldValue: false,
+	characterDataOldValue: false
+});*/
+
+
+
 
 
 /* 
-* CSS that conflicts with the display is ".post_controls .post_controls_inner" because it has fontsize = 0px;
-* By removing that limitation, text-based links will show up.
-*
-* CSS conflicting with the functionality is the "reblog" class. Delete it from the link and it functions as it should.
-* However, this removes the in-window functionality and always opens a new window when clicked.
-*
 * Ideally, the post would open in the current window as a modal on top of the dashboard, not losing your place.
 * However, second best we can do is to just open it in a second tab. I'm not sure where the on-current-screen code stems from.
 */
