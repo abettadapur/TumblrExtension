5f1178b9613350a0c6301fcb96d3e896a7d15b38
Sara Cagle <cagle.sara@gmail.com>
2016-01-04 18:20:25 -0500

    fiddled w comments

--- data/tumblrExt.js
+++ data/tumblrExt.js
@@ -1,16 +1,17 @@
 makeButtons = function(){
 
-	var posts = $(".post_container").not(".new_post_buttons_container"); //gets all posts on the page$
+	var posts = $(".post_container").not(".new_post_buttons_container"); //gets all posts on the page
 	$.each(posts, function(index, post){
 		var postChild = $(".post_full", post).get(0);
-		if(postChild == undefined ){
+		if(postChild == undefined){
 			return;
 		}
 		var postJson = JSON.parse(postChild.attributes["data-json"].value);
 		var rootId = postJson['root_id'];
 		var postId = postJson['id'];
 		var reblogKey = postJson['reblog-key'];
-		var reblogLink ="/reblog/"+rootId+"/"+reblogKey;		
+		var reblogLink ="/reblog/"+rootId+"/"+reblogKey;	
+
 
 		if(rootId != postId) { //if you are NOT the OP, create the new button
 			var buttons_div = $('.post_controls_inner', post).get(0); //grabs the button location
