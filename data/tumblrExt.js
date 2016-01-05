7faa599a7b9aec681b6255f9707be16dbba83972
Alex Bettadapur <alexbettadapur@gmail.com>
2016-01-04 20:46:27 -0500

    fixed scrolling

--- data/tumblrExt.js
+++ data/tumblrExt.js
@@ -1,24 +1,28 @@
-makeButtons = function(){
-
-	var posts = $(".post_container").not(".new_post_buttons_container"); //gets all posts on the page$
-	$.each(posts, function(index, post){
-		var postChild = $(".post_full", post).get(0);
-		if(postChild == undefined ){
-			return;
-		}
-		var postJson = JSON.parse(postChild.attributes["data-json"].value);
-		var rootId = postJson['root_id'];
-		var postId = postJson['id'];
-		var reblogKey = postJson['reblog-key'];
-		var reblogLink ="/reblog/"+rootId+"/"+reblogKey;		
-
-		if(rootId != postId) { //if you are NOT the OP, create the new button
-			var buttons_div = $('.post_controls_inner', post).get(0); //grabs the button location
-			buttons_div.innerHTML = buttons_div.innerHTML + '<a class="post_control post-control-icon reblog-from-source" target="_blank" title="Reblog from Source" href="'+reblogLink+'" data-subview="reblog"></a>'; 
-		}
-	});
+var makeButtons = function(){
+	if(window.location == "https://www.tumblr.com/dashboard")
+	{
+		var posts = $(".post_container").not(".new_post_buttons_container").not("[processed]"); //gets all posts on the page$
+		$.each(posts, function(index, post){
+			var postChild = $(".post_full", post).get(0);
+			if(postChild == undefined ){
+				return;
+			}
+			var postJson = JSON.parse(postChild.attributes["data-json"].value);
+			var rootId = postJson['root_id'];
+			var postId = postJson['id'];
+			var reblogKey = postJson['reblog-key'];
+			var reblogLink ="/reblog/"+rootId+"/"+reblogKey;		
+	
+			if(rootId != postId) { //if you are NOT the OP, create the new button
+				var buttons_div = $('.post_controls_inner', post).get(0); //grabs the button location
+				buttons_div.innerHTML = buttons_div.innerHTML + '<a class="post_control post-control-icon reblog-from-source" target="_blank" title="Reblog from Source" href="'+reblogLink+'" data-subview="reblog"></a>'; 
+			}
+			var typ = document.createAttribute("processed");
+			typ.value = true;
+			post.attributes.setNamedItem(typ);
+		});
+	}
 }
-
 
 
 window.onload = makeButtons();
